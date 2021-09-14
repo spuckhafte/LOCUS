2ecf8f373de797e0fecb282b16d7e85faf0972a8
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2021-09-14 17:03:35 +0530

    Add files via upload

---
+++ components/global.js
@@ -0,0 +1,5 @@
+function login() {
+    // localStorage.setItem('id:', document.getElementById('nameLogin').value)
+    document.getElementById('block2').style.display = "none"
+    document.getElementById('block1').style.display = "block"
+}
