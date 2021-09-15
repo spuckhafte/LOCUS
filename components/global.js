00c4f9152bfb63a090c8aeb2bb9191b5e506ae8f
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2021-09-15 08:37:50 +0530

    Update global.js

--- components/global.js
+++ components/global.js
@@ -1,5 +1,46 @@
+const homeWindow = document.getElementById('block1')
+const loginWindow = document.getElementById('block2')
+const signupWindow = document.getElementById('block3')
+
+window.onload = () => {
+    if (fromLS('id') === null && fromSS('id') === null) {
+        loginWindow.style.display = "block"
+        homeWindow.style.display = "none"
+    } else {
+        loginWindow.style.display = "none"
+        homeWindow.style.display = "block"
+    }
+}
+
 function login() {
-    // localStorage.setItem('id:', document.getElementById('nameLogin').value)
-    document.getElementById('block2').style.display = "none"
-    document.getElementById('block1').style.display = "block"
+    if (document.getElementById('checkRemember').checked) {
+        toLS('id', document.getElementById('userIdLogin').value)
+    } else {
+        toSS('id', document.getElementById('userIdLogin').value)
+    }
+    loginWindow.style.display = "none"
+    homeWindow.style.display = "block"
+}
+
+function signup() {
+    loginWindow.style.display = "none"
+    signupWindow = "block"
+}
+
+
+// use full functions
+function toLS(key, value) {
+    localStorage.setItem(key, value)
+}
+
+function toSS(key, value) {
+    sessionStorage.setItem(key, value)
+}
+
+function fromLS(key) {
+    return localStorage.getItem(key)
+}
+
+function fromSS(key) {
+    return sessionStorage.getItem(key)
 }
